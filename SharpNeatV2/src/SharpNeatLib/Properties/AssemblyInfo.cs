3b0a2a8f748acc4603d0142d540238682ef837ce
colgreen <colin.green1@gmail.com>
2012-04-02 21:55:45 +0000

    Updated assembly version numbers to 2.2.0.0.

--- SharpNeatV2/src/SharpNeatLib/Properties/AssemblyInfo.cs
+++ SharpNeatV2/src/SharpNeatLib/Properties/AssemblyInfo.cs
@@ -27,5 +27,5 @@ using System.Runtime.InteropServices;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.2.0.0")]
+[assembly: AssemblyFileVersion("2.2.0.0")]
