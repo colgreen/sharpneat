4066beb27a8e300283389ac63a0c8cdb9b3ad01c
colgreen <colin.green1@gmail.com>
2012-10-03 19:13:20 +0000

    Updated copyright notices.

--- SharpNeatV2/src/SharpNeatDomainsExtra/Properties/AssemblyInfo.cs
+++ SharpNeatV2/src/SharpNeatDomainsExtra/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("SharpNeatDomainsExtra")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCopyright("Copyright © Colin Green 2004 - 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
