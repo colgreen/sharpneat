6b9cdd14234820f2ce2f018f1c58c31cbcc28ef4
colin.green1@gmail.com <colin.green1@gmail.com>
2016-09-29 22:35:29 +0100

    Updated /clarified licensing information.

--- src/SharpNeatDomains/IntPoint.cs
+++ src/SharpNeatDomains/IntPoint.cs
@@ -3,18 +3,11 @@
  * 
  * Copyright 2004-2016 Colin Green (sharpneat@gmail.com)
  *
- * SharpNEAT is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * SharpNEAT is free software; you can redistribute it and/or modify
+ * it under the terms of The MIT License (MIT).
  *
- * SharpNEAT is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with SharpNEAT.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the MIT License
+ * along with SharpNEAT; if not, see https://opensource.org/licenses/MIT.
  */
 using System;
 namespace SharpNeat.Domains
