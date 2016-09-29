7d9e5b68b42ef96baed56c8e00712d280900e5b3
colin.green1@gmail.com <colin.green1@gmail.com>
2016-09-29 22:40:13 +0100

    Removed last trace of GNU license.

--- src/SharpNeatGUI/Properties/Resources.Designer.cs
+++ src/SharpNeatGUI/Properties/Resources.Designer.cs
@@ -74,20 +74,7 @@ namespace SharpNeatGUI.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to                     GNU GENERAL PUBLIC LICENSE
-        ///                       Version 3, 29 June 2007
-        ///
-        /// Copyright (C) 2007 Free Software Foundation, Inc. &lt;http://fsf.org/&gt;
-        /// Everyone is permitted to copy and distribute verbatim copies
-        /// of this license document, but changing it is not allowed.
-        ///
-        ///                            Preamble
-        ///
-        ///  The GNU General Public License is a free, copyleft license for
-        ///software and other kinds of works.
-        ///
-        ///  The licenses for most software and other practical works are designed
-        ///to take away yo [rest of string was truncated]&quot;;.
+        ///   Looks up a localized string containing licensing info.
         /// </summary>
         internal static string Licensing {
             get {
