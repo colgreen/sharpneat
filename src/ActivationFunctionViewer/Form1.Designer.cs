497b23bcc0c28388d6bd910fe8f37489138aa677
colin.green1@gmail.com <colin.green1@gmail.com>
2017-06-03 13:51:55 +0100

    New app for plotting activation functions.

---
+++ src/ActivationFunctionViewer/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace ActivationFunctionViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.zed = new ZedGraph.ZedGraphControl();
+            this.SuspendLayout();
+            // 
+            // zed
+            // 
+            this.zed.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.zed.Location = new System.Drawing.Point(0, 0);
+            this.zed.Name = "zed";
+            this.zed.ScrollGrace = 0D;
+            this.zed.ScrollMaxX = 0D;
+            this.zed.ScrollMaxY = 0D;
+            this.zed.ScrollMaxY2 = 0D;
+            this.zed.ScrollMinX = 0D;
+            this.zed.ScrollMinY = 0D;
+            this.zed.ScrollMinY2 = 0D;
+            this.zed.Size = new System.Drawing.Size(713, 591);
+            this.zed.TabIndex = 2;
+            this.zed.UseExtendedPrintDialog = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(713, 591);
+            this.Controls.Add(this.zed);
+            this.Name = "Form1";
+            this.Text = "Activation Function Viewer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zed;
+    }
+}
+
---
+++ src/ActivationFunctionViewer/Form1.cs
@@ -0,0 +1,52 @@
+﻿using ActivationFnBenchmarks;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace ActivationFunctionViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Init(Functions.ArcTan);
+        }
+
+        private void Init(Func<double,double> fn)
+        {
+            const double xmin = -100.0;
+            const double xmax = 100;
+            const int resolution = 80000;
+
+            zed.IsShowPointValues = true;
+            zed.PointValueFormat = "e";
+            GraphPane pane = zed.GraphPane;
+
+            pane.XAxis.MajorGrid.IsVisible = true;
+            pane.YAxis.MajorGrid.IsVisible = true;
+
+            pane.Title.Text = "Activation Function";
+            pane.YAxis.Title.Text = "";
+            pane.XAxis.Title.Text = "";
+
+            double[] xarr = new double[resolution];
+            double[] yarr = new double[resolution];
+
+            double incr = (xmax - xmin) / resolution;
+            double x = xmin;
+
+            for(int i=0; i < resolution; i++, x+=incr)
+            {
+                xarr[i] = x;
+                yarr[i] = fn(x);
+            }
+
+            PointPairList list1 = new PointPairList(xarr, yarr);
+            LineItem li = pane.AddCurve("Actual", list1, Color.Red, SymbolType.None);
+
+            zed.AxisChange();
+        }
+    }
+}
