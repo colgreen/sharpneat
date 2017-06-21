2ebd865f080bcdedc8ee760b6772adaaab658b3b
colin.green1@gmail.com <colin.green1@gmail.com>
2017-06-21 23:14:20 +0100

    Method for dumping x,y coords of functions to a CSV file.

--- src/ActivationFunctionViewer/Form1.cs
+++ src/ActivationFunctionViewer/Form1.cs
@@ -11,14 +11,15 @@ namespace ActivationFunctionViewer
         public Form1()
         {
             InitializeComponent();
-            Init(Functions.ArcTan);
+            Init(Functions.LogisticApproximantSteep);
+            //WriteToCsv(Functions.LogisticApproximantSteep);
         }
 
         private void Init(Func<double,double> fn)
         {
-            const double xmin = -100.0;
-            const double xmax = 100;
-            const int resolution = 80000;
+            const double xmin = -2.0;
+            const double xmax = 2.0;
+            const int resolution = 2000;
 
             zed.IsShowPointValues = true;
             zed.PointValueFormat = "e";
@@ -48,5 +49,35 @@ namespace ActivationFunctionViewer
 
             zed.AxisChange();
         }
+
+        //private static void WriteToCsv(Func<double, double> fn)
+        //{
+        //    const double xmin = -2.0;
+        //    const double xmax = 2.0;
+        //    const int resolution = 2000;
+
+        //    double[] xarr = new double[resolution];
+        //    double[] yarr = new double[resolution];
+
+        //    double incr = (xmax - xmin) / resolution;
+        //    double x = xmin;
+
+        //    for (int i = 0; i < resolution; i++, x += incr)
+        //    {
+        //        xarr[i] = x;
+        //        yarr[i] = fn(x);
+        //    }
+
+        //    using (var sw = new StreamWriter("fn.csv"))
+        //    using (var csvWriter = new CsvWriter(sw))
+        //    {
+        //        // Header row.
+        //        csvWriter.WriteRecord("x", "y");
+
+        //        for(int i=0; i< xarr.Length; i++) {
+        //            csvWriter.WriteRecord(xarr[i].ToString(), yarr[i].ToString());
+        //        }
+        //    }
+        //}
     }
 }
