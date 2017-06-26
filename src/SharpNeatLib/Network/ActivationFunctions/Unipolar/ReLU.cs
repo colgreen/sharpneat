7fc6b5210690e166ef6dca2116d199d0ea584d47
colin.green1@gmail.com <colin.green1@gmail.com>
2017-06-26 13:46:22 +0100

    Added ReLU activation function.

---
+++ src/SharpNeatLib/Network/ActivationFunctions/Unipolar/ReLU.cs
@@ -0,0 +1,66 @@
+﻿/* ***************************************************************************
+ * This file is part of SharpNEAT - Evolution of Neural Networks.
+ * 
+ * Copyright 2004-2016 Colin Green (sharpneat@gmail.com)
+ *
+ * SharpNEAT is free software; you can redistribute it and/or modify
+ * it under the terms of The MIT License (MIT).
+ *
+ * You should have received a copy of the MIT License
+ * along with SharpNEAT; if not, see https://opensource.org/licenses/MIT.
+ */
+using Redzen.Numerics;
+
+namespace SharpNeat.Network
+{
+    /// <summary>
+    /// Rectified linear activation unit (ReLU).
+    /// </summary>
+    public class ReLU : IActivationFunction
+    {
+        /// <summary>
+        /// Default instance provided as a public static field.
+        /// </summary>
+        public static readonly IActivationFunction __DefaultInstance = new ReLU();
+
+        public string FunctionId => this.GetType().Name;
+
+        public string FunctionString => "";
+
+        public string FunctionDescription => "Leaky Rectified Linear Unit (ReLU)";
+
+        public bool AcceptsAuxArgs => false;
+
+        public double Calculate(double x, double[] auxArgs)
+        {
+            double y;
+            if (x > 0.0) {
+                y = x;
+            } else {
+                y = 0;
+            }
+            return y;
+        }
+
+        public float Calculate(float x, float[] auxArgs)
+        {
+            float y;
+            if (x > 0.0) {
+                y = x;
+            } else {
+                y = 0;
+            }
+            return y;
+        }
+
+        public double[] GetRandomAuxArgs(XorShiftRandom rng, double connectionWeightRange)
+        {
+            throw new SharpNeatException("GetRandomAuxArgs() called on activation function that does not use auxiliary arguments.");
+        }
+
+        public void MutateAuxArgs(double[] auxArgs, XorShiftRandom rng, ZigguratGaussianSampler gaussianSampler, double connectionWeightRange)
+        {
+            throw new SharpNeatException("MutateAuxArgs() called on activation function that does not use auxiliary arguments.");
+        }
+    }
+}
