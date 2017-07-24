f206323bda20460b797536598b913c79a3956c95
Colin Green <colin.green1@gmail.com>
2017-07-24 20:52:31 +0100

    AcyclicNeuralNetTests (not implemented).

---
+++ src/SharpNeatLib.Tests/Phenomes/NeuralNets/AcyclicNeuralNetTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpNeat.Network2;
+using SharpNeat.Network2.Acyclic;
+using SharpNeat.Phenomes.NeuralNets;
+
+namespace SharpNeatLib.Tests.Phenomes.NeuralNets
+{
+
+    [TestClass]
+    public class AcyclicNeuralNetTests
+    {
+
+        #region Test Methods
+
+        [TestMethod]
+        [TestCategory("AcyclicNeuralNet")]
+        public void SingleInputOutput()
+        {
+            var connList = new List<IWeightedDirectedConnection<double>>();
+            connList.Add(new WeightedDirectedConnection<double>(0, 1, 0.0));
+
+
+
+
+
+            // Create graph.
+            var digraph = WeightedAcyclicDirectedGraphFactory<double>.Create(connList, 1, 1);
+
+
+
+            //new AcyclicNeuralNet(digraph, SteepenedSigmoid)
+            
+        }
+
+        #endregion
+
+
+    }
+
+
+
+}
