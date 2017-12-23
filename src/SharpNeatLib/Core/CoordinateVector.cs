05c301ec409886e701641cdfd01658042bee6c37
colin.green1@gmail.com <colin.green1@gmail.com>
2017-12-23 22:24:51 +0000

    Debug assertion test fix.

--- src/SharpNeatLib/Core/CoordinateVector.cs
+++ src/SharpNeatLib/Core/CoordinateVector.cs
@@ -63,6 +63,7 @@ namespace SharpNeat.Core
                 if(coordElemArray[i].Key <= prevId) {
                     return false;
                 }
+                prevId = coordElemArray[i].Key;
             }
             return true;
         }
