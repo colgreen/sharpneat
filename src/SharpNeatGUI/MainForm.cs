a0144d338e4972a879f6489ea4532932279d54c8
colin.green1@gmail.com <colin.green1@gmail.com>
2018-05-12 22:15:48 +0100

    Set the default culture for all threads in the application to the Invariant culture. This is a cheap way of ensuring that all form fields and data IO routines read and write textual data in the same format, in particular the use of a dot as the decimal separator (some culture use a comma).
    This addresses issue #26

--- src/SharpNeatGUI/MainForm.cs
+++ src/SharpNeatGUI/MainForm.cs
@@ -82,6 +82,12 @@ namespace SharpNeatGUI
         /// </summary>
         public MainForm()
         {
+            // Set the default culture for all threads in the application to the Invariant culture.
+            // This is a cheap way of ensuring that all form fields and data IO routines
+            // read and write textual data in the same format, in particular the use of a dot as the
+            // decimal separator (some culture use a comma).
+            CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+
             InitializeComponent();
             Logger.SetListBox(lbxLog);
             XmlConfigurator.Configure(new FileInfo("log4net.properties"));
