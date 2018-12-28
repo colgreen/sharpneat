ce95d4f94ff02772465b12d728e4e539cc1505a1
Colin Green <colin.green1@gmail.com>
2018-12-28 18:36:07 +0000

    Partial implementation of PerformOneGeneration().

--- src/SharpNeatLib/Neat/EvolutionAlgorithm/NeatEvolutionAlgorithm.cs
+++ src/SharpNeatLib/Neat/EvolutionAlgorithm/NeatEvolutionAlgorithm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using Redzen.Numerics;
 using Redzen.Random;
@@ -129,37 +130,32 @@ namespace SharpNeat.Neat.EvolutionAlgorithm
 
         public void PerformOneGeneration()
         {
-            // TODO: Implement! Rough plan below...
-
-
             // Create offspring.
             List<NeatGenome<T>> offspringList = _offspringBuilder.CreateOffspring(_pop.SpeciesArray, _rng);
 
-
             // Trim population back to elite genomes only.
+            // Get back a flag that indicates if one or more species are now completely empty (i.e. species.genomeList.Count == 0).
+            bool emptySpeciesFlag = TrimSpeciesBackToElite();
+
+            // Rebuild _pop.GenomeList. It will now contain just the elite genomes from each species.
+            RebuildGenomeList();
+
+            // Append offspring genomes to the elite genomes in _pop.GenomeList. We do this before calling the
+            // _genomeListEvaluator.Evaluate() because some evaluation schemes re-evaluate the elite genomes 
+            // (otherwise we could just evaluate offspringList).
+            _pop.GenomeList.AddRange(offspringList);
+
+            // Evaluate all of the genomes in the population, assigning fitness info to each.
+            _evaluator.Evaluate(_pop.GenomeList);
+
+            // Integrate offspring into the species.
+            IntegrateOffspringIntoSpecies(offspringList, emptySpeciesFlag);
 
 
-            // Merge offspring into genomeList (and species?)
 
-
-            // Evaluate all genomes in genomeList.
-
+            // TODO: Finish off.
 
             // Update population and per-species stats
-
-
-
-
-
-
-
-
-            //// Evaluate each genome in the population; assigning fitness info to each (a single fitness score,
-            //// or perhaps a series of scores each measuring a different aspect of fitness).
-            //_evaluator.Evaluate(_pop.GenomeList);
-
-            //// Invoke the reproduction strategy (select, cull, create offspring).
-            //_selectionReproStrategy.Invoke(_pop);
 
             //// Update stats.
             //UpdateBestGenome();
@@ -172,7 +168,101 @@ namespace SharpNeat.Neat.EvolutionAlgorithm
 
         #region Private Methods
 
-        
+        /// <summary>
+        /// Trims the genomeList in each specie back to the number of elite genomes defined for each species.
+        /// Returns true if there are empty species following trimming.
+        /// </summary>
+        /// <remarks>
+        /// The genomes in species.GenomeList are ordered best to worst, thus the genomes must be removed from the end of the list.
+        /// </remarks>
+        private bool TrimSpeciesBackToElite()
+        {
+            bool emptySpeciesFlag = false;
+            int speciesCount = _pop.SpeciesArray.Length;
+
+            for(int i=0; i < speciesCount; i++)
+            {
+                Species<T> species = _pop.SpeciesArray[i];
+                int eliteSizeInt = species.Stats.EliteSizeInt;
+                int removeCount = species.GenomeList.Count - eliteSizeInt;
+                species.GenomeList.RemoveRange(eliteSizeInt, removeCount);
+
+                if(0 == eliteSizeInt) {
+                    emptySpeciesFlag = true;
+                }
+            }
+
+            return emptySpeciesFlag;
+        }
+
+        /// <summary>
+        /// Rebuild _genomeList from genomes held within the species.
+        /// </summary>
+        private void RebuildGenomeList()
+        {
+            var genomeList = _pop.GenomeList;
+
+            genomeList.Clear();
+            foreach(var species in _pop.SpeciesArray) {
+                genomeList.AddRange(species.GenomeList);
+            }
+        }
+
+        private void IntegrateOffspringIntoSpecies(List<NeatGenome<T>> offspringList, bool emptySpeciesFlag)
+        {
+            // Handle special case of one or more species having a zero target size (dead species).
+            if(emptySpeciesFlag)
+            {   
+                // There are one or more terminated species. Therefore we need to fully re-speciate all genomes 
+                // to evenly divide them between the required number of species.
+
+                // Clear all genomes from species (note. we still have all genomes in _pop.GenomeList).
+                ClearAllSpecies();
+
+                // Re-initialise the species.
+                _pop.InitialiseSpecies(_speciationStrategy, _eaSettings.SpeciesCount, _rng);    
+            }
+            else
+            {
+                // Integrate offspring into the existing species. 
+                _speciationStrategy.SpeciateAdd(offspringList, _pop.SpeciesArray, _rng);
+
+                // Sort the genomes in each species. Highest fitness first, then secondary sorted by youngest genomes first.
+                foreach(var species in _pop.SpeciesArray) {
+                    SortUtils.SortUnstable(species.GenomeList, GenomeFitnessAndAgeComparer<T>.Singleton, _rng);
+                }
+            }
+
+            Debug.Assert(!TestForEmptySpecies(), "Speciation resulted in one or more empty species.");
+        }
+
+        #endregion
+
+        #region Private Methods [Low Level]
+
+        /// <summary>
+        /// Clear the genome list of all species.
+        /// </summary>
+        private void ClearAllSpecies()
+        {
+            foreach(var species in _pop.SpeciesArray) {
+                species.GenomeList.Clear();
+            }
+        }
+
+        /// <summary>
+        /// Returns true if there is one or more empty species.
+        /// </summary>
+        private bool TestForEmptySpecies()
+        {
+            foreach(var species in _pop.SpeciesArray) {
+                if(species.GenomeList.Count == 0) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
         #endregion
     }
 }
