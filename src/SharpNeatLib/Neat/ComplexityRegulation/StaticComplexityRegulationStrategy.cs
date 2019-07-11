696cdfb35d315819810520d27d7a1d07091b57f0
colgreen <colin.green1@gmail.com>
2019-07-11 23:27:32 +0100

    Added StaticComplexityRegulationStrategy.

---
+++ src/SharpNeatLib/Neat/ComplexityRegulation/StaticComplexityRegulationStrategy.cs
@@ -0,0 +1,56 @@
+﻿/* ***************************************************************************
+ * This file is part of SharpNEAT - Evolution of Neural Networks.
+ * 
+ * Copyright 2004-2019 Colin Green (sharpneat@gmail.com)
+ *
+ * SharpNEAT is free software; you can redistribute it and/or modify
+ * it under the terms of The MIT License (MIT).
+ *
+ * You should have received a copy of the MIT License
+ * along with SharpNEAT; if not, see https://opensource.org/licenses/MIT.
+ */
+using SharpNeat.EvolutionAlgorithm;
+
+namespace SharpNeat.Neat.ComplexityRegulation
+{
+    /// <summary>
+    /// A complexity regulation strategy that remains fixed in a single complexity regulation mode
+    /// throughout the lifetime of the evolution algorithm.
+    /// </summary>
+    public class StaticComplexityRegulationStrategy : IComplexityRegulationStrategy
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Construct with the given sttaic mode.
+        /// </summary>
+        /// <param name="mode">The static mode.</param>
+        public StaticComplexityRegulationStrategy(ComplexityRegulationMode mode)
+        {
+            this.CurrentMode = mode;
+        }
+
+        #endregion
+
+        #region IComplexityRegulationStrategy
+
+        /// <summary>
+        /// Gets the current complexity regulation mode.
+        /// </summary>
+        public ComplexityRegulationMode CurrentMode { get; }
+
+        /// <summary>
+        /// Update the complexity regulation mode that the evolution algorithm should be in.
+        /// </summary>
+        /// <param name="eaStats">Evolution algorithm statistics.</param>
+        /// <param name="popStats">Population statistics.</param>
+        /// <returns>The updated mode.</returns>
+        public ComplexityRegulationMode UpdateMode(EvolutionAlgorithmStatistics eaStats, PopulationStatistics popStats)
+        {
+            // This is the static strategy, therefore do nothing.
+            return this.CurrentMode;
+        }
+
+        #endregion
+    }
+}
