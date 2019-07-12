de708998d660f192935579274d3c161b7efe8623
colgreen <colin.green1@gmail.com>
2019-07-12 12:07:46 +0100

    Added INeatExperiment, NeatExperiment.

---
+++ src/SharpNeatLib/Experiments/INeatExperiment.cs
@@ -0,0 +1,104 @@
+﻿/* ***************************************************************************
+ * This file is part of SharpNEAT - Evolution of Neural Networks.
+ * 
+ * Copyright 2004-2019 Colin Green (sharpneat@gmail.com)
+ *
+ * SharpNEAT is free software; you can redistribute it and/or modify
+ * it under the terms of The MIT License (MIT).
+ *
+ * You should have received a copy of the MIT License
+ * along with SharpNEAT; if not, see https://opensource.org/licenses/MIT.
+ */
+using SharpNeat.Evaluation;
+using SharpNeat.Neat.ComplexityRegulation;
+using SharpNeat.Neat.EvolutionAlgorithm;
+using SharpNeat.Neat.Genome;
+using SharpNeat.Neat.Reproduction.Asexual;
+using SharpNeat.Neat.Reproduction.Sexual;
+using SharpNeat.Neat.Speciation;
+
+namespace SharpNeat.Experiments
+{
+    /// <summary>
+    /// An interface that brings together a number of settings objects that make up a given experiment.
+    /// </summary>
+    /// <typeparam name="T">Black box numeric data type.</typeparam>
+    public interface INeatExperiment<T> where T : struct
+    {
+        /// <summary>
+        /// Experiment name.
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// Experiment description.
+        /// </summary>
+        string Description { get; set; }
+
+        /// <summary>
+        /// Experiment evaluation scheme object.
+        /// </summary>
+        IBlackBoxEvaluationScheme<T> EvaluationScheme { get; }
+
+        /// <summary>
+        /// A boolean flag that indicates if the genomes that are evolved are acyclic,
+        /// i.e. they should have no recurrent/cyclic connection paths.
+        /// </summary>
+        bool IsAcyclic { get; set; }
+
+        /// <summary>
+        /// For cyclic neural networks (i.e. if <see cref="IsAcyclic"/> is false) this defines how many timesteps to run the neural net per call to Activate().
+        /// </summary>
+        int CyclesPerActivation { get; set; }
+
+        /// <summary>
+        /// Name of the neuron activation function to use in evolved networks.
+        /// </summary>
+        string ActivationFnName { get; set; } 
+
+        /// <summary>
+        /// The <see cref="NeatEvolutionAlgorithmSettings"/> to be used for the experiment.
+        /// </summary>
+        NeatEvolutionAlgorithmSettings NeatEvolutionAlgorithmSettings { get; }
+
+        /// <summary>
+        /// The asexual reproduction settings to use for the experiment.
+        /// </summary>
+        NeatReproductionAsexualSettings ReproductionAsexualSettings { get; }
+
+        /// <summary>
+        /// The sexual reproduction settings to use for the experiment.
+        /// </summary>
+        NeatReproductionSexualSettings ReproductionSexualSettings { get; }
+  
+        /// <summary>
+        /// The population size to use for the experiment.
+        /// </summary>
+        int PopulationSize { get; set; }
+
+        /// <summary>
+        /// The initial interconnections proportion. This is the proportion of possible
+        /// direct connections from the input nodes to the output nodes that are to be created in 
+        /// each initial genome. The connections to create are selected at random (using a 
+        /// select-without-replacement method).
+        /// </summary>
+        double InitialInterconnectionsProportion { get; set; }
+
+        /// <summary>
+        /// The maximum connection weight scale/magnitude. 
+        /// E.g. a value of 5 defines a weight range of -5 to 5.
+        /// The weight range is strictly enforced, e.g. when creating new connections and mutating existing ones.
+        /// </summary>
+        double ConnectionWeightScale { get; set; }
+
+        /// <summary>
+        /// The complexity regulation strategy to use for the experiment.
+        /// </summary>
+        IComplexityRegulationStrategy ComplexityRegulationStrategy { get; set; }
+
+        /// <summary>
+        /// The speciation strategy to use for the experiment.
+        /// </summary>
+        ISpeciationStrategy<NeatGenome<double>,double> SpeciationStrategy { get; set; }
+    }
+}
---
+++ src/SharpNeatLib/Experiments/NeatExperiment.cs
@@ -0,0 +1,191 @@
+﻿/* ***************************************************************************
+ * This file is part of SharpNEAT - Evolution of Neural Networks.
+ * 
+ * Copyright 2004-2019 Colin Green (sharpneat@gmail.com)
+ *
+ * SharpNEAT is free software; you can redistribute it and/or modify
+ * it under the terms of The MIT License (MIT).
+ *
+ * You should have received a copy of the MIT License
+ * along with SharpNEAT; if not, see https://opensource.org/licenses/MIT.
+ */
+using System;
+using SharpNeat.Evaluation;
+using SharpNeat.Neat.ComplexityRegulation;
+using SharpNeat.Neat.DistanceMetrics;
+using SharpNeat.Neat.DistanceMetrics.Double;
+using SharpNeat.Neat.EvolutionAlgorithm;
+using SharpNeat.Neat.Genome;
+using SharpNeat.Neat.Reproduction.Asexual;
+using SharpNeat.Neat.Reproduction.Sexual;
+using SharpNeat.Neat.Speciation;
+
+namespace SharpNeat.Experiments
+{
+    /// <summary>
+    /// An aggregation of settings objects that make up a given experiment.
+    /// </summary>
+    ///<typeparam name="T">Black box numeric data type.</typeparam>
+    public class NeatExperiment<T> : INeatExperiment<T>
+        where T : struct
+    {
+        #region Auto Properties
+
+        /// <summary>
+        /// Experiment name.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Experiment description.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Experiment evaluation scheme object.
+        /// </summary>
+        public IBlackBoxEvaluationScheme<T> EvaluationScheme { get; }
+
+        /// <summary>
+        /// A boolean flag that indicates if the genomes that are evolved are acyclic,
+        /// i.e. they should have no recurrent/cyclic connection paths.
+        /// </summary>
+        public bool IsAcyclic { get; set; }
+
+        /// <summary>
+        /// For cyclic neural networks (i.e. if <see cref="IsAcyclic"/> is false) this defines how many timesteps to run the neural net per call to Activate().
+        /// </summary>
+        public int CyclesPerActivation { get; set; }
+
+        /// <summary>
+        /// Name of the neuron activation function to use in evolved networks.
+        /// </summary>
+        public string ActivationFnName { get; set; } 
+
+        /// <summary>
+        /// The <see cref="NeatEvolutionAlgorithmSettings"/> to be used for the experiment.
+        /// </summary>
+        public NeatEvolutionAlgorithmSettings NeatEvolutionAlgorithmSettings { get; }
+
+        /// <summary>
+        /// The asexual reproduction settings to use for the experiment.
+        /// </summary>
+        public NeatReproductionAsexualSettings ReproductionAsexualSettings { get; }
+
+        /// <summary>
+        /// The sexual reproduction settings to use for the experiment.
+        /// </summary>
+        public NeatReproductionSexualSettings ReproductionSexualSettings { get; }
+
+        /// <summary>
+        /// The population size to use for the experiment.
+        /// </summary>
+        public int PopulationSize { get; set; }
+
+        /// <summary>
+        /// The initial interconnections proportion. This is the proportion of possible
+        /// direct connections from the input nodes to the output nodes that are to be created in 
+        /// each initial genome. The connections to create are selected at random (using a 
+        /// select-without-replacement method).
+        /// </summary>
+        public double InitialInterconnectionsProportion { get; set; }
+
+        /// <summary>
+        /// The maximum connection weight scale/magnitude. 
+        /// E.g. a value of 5 defines a weight range of -5 to 5.
+        /// The weight range is strictly enforced, e.g. when creating new connections and mutating existing ones.
+        /// </summary>
+        public double ConnectionWeightScale { get; set; }
+
+        /// <summary>
+        /// The complexity regulation strategy to use for the experiment.
+        /// </summary>
+        public IComplexityRegulationStrategy ComplexityRegulationStrategy { get; set; }
+
+        /// <summary>
+        /// The speciation strategy to use for the experiment.
+        /// </summary>
+        public ISpeciationStrategy<NeatGenome<double>,double> SpeciationStrategy { get; set; }
+        
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Constructs with the provided name and evaluation scheme, and default settings.
+        /// </summary>
+        /// <param name="name">Experiment name.</param>
+        /// <param name="evalScheme">Experiment evaluation scheme object.</param>
+        /// <param name="activationFnName">Name of the neuron activation function to use in evolved networks.</param>
+        /// <param name="isAcyclic">Indicates if the genomes that are evolved are to be acyclic.</param>
+        /// <param name="cyclesPerActivation">For cyclic neural networks (i.e. if <see cref="isAcyclic"/> is false) this defines how many timesteps to run the neural net per call to Activate().</param>
+        private NeatExperiment(
+            string name,
+            IBlackBoxEvaluationScheme<T> evalScheme,
+            string activationFnName,
+            bool isAcyclic,
+            int cyclesPerActivation)
+        {
+            this.Name = name ?? throw new ArgumentNullException(nameof(name));
+            this.EvaluationScheme = evalScheme ?? throw new ArgumentNullException(nameof(evalScheme));
+            this.IsAcyclic = isAcyclic;
+            this.CyclesPerActivation = cyclesPerActivation;
+            this.ActivationFnName = activationFnName ?? throw new ArgumentNullException(nameof(activationFnName));
+
+            // Assign a set of default settings.
+            this.NeatEvolutionAlgorithmSettings = new NeatEvolutionAlgorithmSettings();
+            this.ReproductionAsexualSettings = new NeatReproductionAsexualSettings();
+            this.ReproductionSexualSettings = new NeatReproductionSexualSettings();
+            this.PopulationSize = 400;
+            this.InitialInterconnectionsProportion = 0.05;
+            this.ConnectionWeightScale = 5.0;
+
+            // Assign a default complexity regulation strategy.
+            this.ComplexityRegulationStrategy = new NullComplexityRegulationStrategy();
+
+            // Assign a default speciation strategy.
+            IDistanceMetric<double> distanceMetric = new ManhattanDistanceMetric(1.0, 0.0, 10.0);
+            this.SpeciationStrategy = new Neat.Speciation.GeneticKMeans.Parallelized.GeneticKMeansSpeciationStrategy<double>(distanceMetric, 5);
+        }
+
+        #endregion
+
+        #region Public Static Factory Methods
+
+        /// <summary>
+        /// Create a new instance of <see cref="NeatExperiment{T}"/> for experiments with acyclic neural networks, and a set of
+        /// default settings.
+        /// </summary>
+        /// <param name="name">Experiment name.</param>
+        /// <param name="evalScheme">Experiment evaluation scheme object.</param>
+        /// <param name="activationFnName">Name of the neuron activation function to use in evolved networks.</param>
+        /// <returns>A new instance of <see cref="NeatExperiment{T}"/>.</returns>
+        public static NeatExperiment<T> CreateAcyclic(
+            string name,
+            IBlackBoxEvaluationScheme<T> evalScheme,
+            string activationFnName)
+        {
+            return new NeatExperiment<T>(name, evalScheme, activationFnName, true, 0);
+        }
+
+        /// <summary>
+        /// Create a new instance of <see cref="NeatExperiment{T}"/> for experiments with cyclic neural networks, and a set of
+        /// default settings.
+        /// </summary>
+        /// <param name="name">Experiment name.</param>
+        /// <param name="evalScheme">Experiment evaluation scheme object.</param>
+        /// <param name="activationFnName">Name of the neuron activation function to use in evolved networks.</param>
+        /// <param name="cyclesPerActivation">For cyclic neural networks (i.e. if <see cref="isAcyclic"/> is false) this defines how many timesteps to run the neural net per call to Activate().</param>
+        /// <returns>A new instance of <see cref="NeatExperiment{T}"/>.</returns>
+        public static NeatExperiment<T> CreateCyclic(
+            string name,
+            IBlackBoxEvaluationScheme<T> evalScheme,
+            string activationFnName,
+            int cyclesPerActivation)
+        {
+            return new NeatExperiment<T>(name, evalScheme, activationFnName, false, cyclesPerActivation);
+        }
+
+        #endregion
+    }
+}
