8523a52101adf41bf8ec97fc9811d97275471771
colgreen <colin.green1@gmail.com>
2019-07-14 11:24:21 +0100

    JsonReadMandatoryUtils: Added ReadBoolMandatory(), ReadDoubleMandatory(), ReadStringMandatory().

--- src/SharpNeatLib/IO/JsonReadMandatoryUtils.cs
+++ src/SharpNeatLib/IO/JsonReadMandatoryUtils.cs
@@ -19,6 +19,25 @@ namespace SharpNeat.IO
     /// </summary>
     public static class JsonReadMandatoryUtils
     {
+        #region Public Static Methods [Read Mandatory Properties]
+
+        /// <summary>
+        /// Read a mandatory boolean property; if not present then an exception is throw
+        /// </summary>
+        /// <param name="jobj">The json object to read the property from.</param>
+        /// <param name="propertyName">The name of the json property to read.</param>
+        /// <returns>The property value</returns>
+        public static bool ReadBoolMandatory(
+            JObject jobj,
+            string propertyName)
+        {
+            bool? val = (bool?)jobj[propertyName];
+            if(val.HasValue) {
+                return val.Value;
+            }
+            throw new Exception($"Missing mandatory property [{propertyName}]");
+        }
+
         /// <summary>
         /// Read a mandatory integer property; if not present then an exception is throw
         /// </summary>
@@ -35,5 +54,41 @@ namespace SharpNeat.IO
             }
             throw new Exception($"Missing mandatory property [{propertyName}]");
         }
+
+        /// <summary>
+        /// Read a mandatory double precision floating point property; if not present then an exception is throw
+        /// </summary>
+        /// <param name="jobj">The json object to read the property from.</param>
+        /// <param name="propertyName">The name of the json property to read.</param>
+        /// <returns>The property value</returns>
+        public static double ReadDoubleMandatory(
+            JObject jobj,
+            string propertyName)
+        {
+            double? val = (double?)jobj[propertyName];
+            if(val.HasValue) {
+                return val.Value;
+            }
+            throw new Exception($"Missing mandatory property [{propertyName}]");
+        }
+
+        /// <summary>
+        /// Read a mandatory string property; if not present then an exception is throw
+        /// </summary>
+        /// <param name="jobj">The json object to read the property from.</param>
+        /// <param name="propertyName">The name of the json property to read.</param>
+        /// <returns>The property value</returns>
+        public static string ReadStringMandatory(
+            JObject jobj,
+            string propertyName)
+        {
+            string val = (string)jobj[propertyName];
+            if(val != null) {
+                return val;
+            }
+            throw new Exception($"Missing mandatory property [{propertyName}]");
+        }
+
+        #endregion
     }
 }
--- src/Tests/SharpNeatLib.Tests/IO/JsonReadMandatoryUtilsTests.cs
+++ src/Tests/SharpNeatLib.Tests/IO/JsonReadMandatoryUtilsTests.cs
@@ -9,6 +9,23 @@ namespace SharpNeatLib.Tests.IO
     public class JsonReadMandatoryUtilsTests
     {
         [TestMethod]
+        public void ReadBoolMandatory()
+        {
+            JObject jobj = JObject.Parse("{'foo':true}");
+
+            // Test success case.
+            bool val = JsonReadMandatoryUtils.ReadBoolMandatory(jobj, "foo");
+            Assert.AreEqual(true, val);
+
+            // Test missing mandatory.
+            Assert.ThrowsException<Exception>(() => JsonReadMandatoryUtils.ReadBoolMandatory(jobj, "bar"));
+
+            // Test invalid value.
+            jobj = JObject.Parse("{'foo':'abc'}");
+            Assert.ThrowsException<FormatException>(() => JsonReadMandatoryUtils.ReadBoolMandatory(jobj, "foo"));
+        }
+
+        [TestMethod]
         public void ReadIntMandatory()
         {
             JObject jobj = JObject.Parse("{'foo':'1234'}");
@@ -23,6 +40,36 @@ namespace SharpNeatLib.Tests.IO
             // Test invalid value.
             jobj = JObject.Parse("{'foo':'abc'}");
             Assert.ThrowsException<FormatException>(() => JsonReadMandatoryUtils.ReadIntMandatory(jobj, "foo"));
+        }
+
+        [TestMethod]
+        public void ReadDoubleMandatory()
+        {
+            JObject jobj = JObject.Parse("{'foo':'1234.5'}");
+
+            // Test success case.
+            double val = JsonReadMandatoryUtils.ReadDoubleMandatory(jobj, "foo");
+            Assert.AreEqual(1234.5, val);
+
+            // Test missing mandatory.
+            Assert.ThrowsException<Exception>(() => JsonReadMandatoryUtils.ReadDoubleMandatory(jobj, "bar"));
+
+            // Test invalid value.
+            jobj = JObject.Parse("{'foo':'abc'}");
+            Assert.ThrowsException<FormatException>(() => JsonReadMandatoryUtils.ReadDoubleMandatory(jobj, "foo"));
+        }
+
+        [TestMethod]
+        public void ReadStringMandatory()
+        {
+            JObject jobj = JObject.Parse("{'foo':'hello world'}");
+
+            // Test success case.
+            string val = JsonReadMandatoryUtils.ReadStringMandatory(jobj, "foo");
+            Assert.AreEqual("hello world", val);
+
+            // Test missing mandatory.
+            Assert.ThrowsException<Exception>(() => JsonReadMandatoryUtils.ReadStringMandatory(jobj, "bar"));
         }
     }
 }
