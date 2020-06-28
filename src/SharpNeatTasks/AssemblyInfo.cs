1d5c97eb8b336857a7c6ffd34ad96209969afe19
colgreen <colin.green1@gmail.com>
2020-06-28 18:23:06 +0100

    Renamed SharpNeatLib.Benchmarks to SharpNeat.Benchmarks

--- src/SharpNeatTasks/AssemblyInfo.cs
+++ src/SharpNeatTasks/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly:InternalsVisibleTo("SharpNeatTasks.Benchmarks")]
+[assembly:InternalsVisibleTo("SharpNeat.Tasks.Benchmarks")]
 [assembly:InternalsVisibleTo("SharpNeatTasks.Tests")]
