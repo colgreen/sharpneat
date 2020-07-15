10f733bed7a62a1512ef101e2a42fb7167a11c33
colgreen <colin.green1@gmail.com>
2020-07-15 20:51:32 +0100

    Assembly version update: 2.4.1.5 -> 2.4.4.0

--- src/SharpNeatGUI/Properties/AssemblyInfo.cs
+++ src/SharpNeatGUI/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("2.4.0.0")]
-[assembly: AssemblyFileVersion("2.4.0.0")]
+[assembly: AssemblyVersion("2.4.4.0")]
+[assembly: AssemblyFileVersion("2.4.4.0")]
