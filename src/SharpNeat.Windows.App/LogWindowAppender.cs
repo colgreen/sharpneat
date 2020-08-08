259cf80263a704ea7bbf2481b1bf91878a00f7f9
colgreen <colin.green1@gmail.com>
2020-08-08 20:41:26 +0100

    Added LogWindowAppender.

---
+++ src/SharpNeat.Windows.App/LogWindowAppender.cs
@@ -0,0 +1,19 @@
+﻿using log4net.Appender;
+using log4net.Core;
+
+namespace SharpNeat.Windows.App
+{
+    /// <summary>
+    /// Log4net appender that redirects log messages to a custom logging system.
+    /// </summary>
+    public class LogWindowAppender : AppenderSkeleton
+    {
+        /// <summary>
+        /// Handle log event.
+        /// </summary>
+        protected override void Append(LoggingEvent loggingEvent)
+        {
+            Logger.Log(RenderLoggingEvent(loggingEvent));   
+        }
+    }
+}
