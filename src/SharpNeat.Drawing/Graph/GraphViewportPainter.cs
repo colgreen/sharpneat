d056b276f3705d600976516242e1e6320f7163ec
colgreen <colin.green1@gmail.com>
2020-09-06 19:57:35 +0100

    - Added GraphViewportPainter. - Deleted GraphControl. - Renamed Viewport to ViewportControl, and reviewed refresh logic.

---
+++ src/SharpNeat.Drawing/Graph/GraphViewportPainter.cs
@@ -0,0 +1,142 @@
+﻿/* ***************************************************************************
+ * This file is part of SharpNEAT - Evolution of Neural Networks.
+ * 
+ * Copyright 2004-2020 Colin Green (sharpneat@gmail.com)
+ *
+ * SharpNEAT is free software; you can redistribute it and/or modify
+ * it under the terms of The MIT License (MIT).
+ *
+ * You should have received a copy of the MIT License
+ * along with SharpNEAT; if not, see https://opensource.org/licenses/MIT.
+ */
+using System.Drawing;
+using SharpNeat.Drawing.Graph.Painting;
+
+namespace SharpNeat.Drawing.Graph
+{
+    /// <summary>
+    /// Paints graphs to a viewport.
+    /// </summary>
+    /// <remarks>
+    /// Graph painting involves two main high level steps 
+    /// 1) laying out the nodes of the graph within the specified paint area.
+    /// 2) Painting the nodes and connections based on the node positions assigned in step 1.
+    /// </remarks>
+    public class GraphViewportPainter : IViewportPainter
+    {
+        static readonly Brush _brushBackground = new SolidBrush(Color.White);
+
+        readonly IGraphLayoutScheme _layoutScheme;
+        readonly GraphPainter _graphPainter;
+
+        // The current graph view model being painted.
+        DirectedGraphViewModel? _graphViewModel;
+
+        // Describes the viewport area the graph has been laid out within. The initial size of -1, -1 indicates no layout has yet occurred.
+        Rectangle _viewportArea = new Rectangle(0, 0, -1, -1);
+        object? _layoutSchemeData;
+
+        #region Construction
+
+        /// <summary>
+        /// Construct with a default layout manager and graph painter.
+        /// </summary>
+        public GraphViewportPainter()
+        {
+            _layoutScheme = new DepthLayoutScheme();
+            _graphPainter = new GraphPainter();
+        }
+
+        /// <summary>
+        /// Construct with the provided layout manager and graph painter.
+        /// </summary>
+        public GraphViewportPainter(
+            IGraphLayoutScheme layoutScheme,
+            GraphPainter graphPainter)
+        {
+            _layoutScheme = layoutScheme;
+            _graphPainter = graphPainter;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the graph view model to paint.
+        /// </summary>
+        public DirectedGraphViewModel? GraphViewModel 
+        { 
+            get => _graphViewModel;
+            set
+            {
+                _graphViewModel = value;
+
+                // Reset viewportArea to indicate no layout of the new graph has yet occurred.
+                _viewportArea = new Rectangle(0, 0, -1, -1);
+
+                // Reset any layout data created by the scheme, as this would have been specific to the previous model.
+                _layoutSchemeData = null;
+            }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Paint the current graph to the provided GDI+ drawing surface.
+        /// </summary>
+        /// <param name="g">The GDI+ drawing surface to paint on to.</param>
+        /// <param name="viewportArea">The area to paint within.</param>
+        /// <param name="zoomFactor">Zoom factor.</param>
+        public void Paint(Graphics g, Rectangle viewportArea, float zoomFactor)
+        {
+            // Paint background as a solid colour.
+            g.FillRectangle(_brushBackground, viewportArea);
+
+            // Get a local stack reference to the class variable; this avoid the chance of another thread changing the model object from this point onwards.
+            var model = this.GraphViewModel;
+
+            // Skip painting if nothing to paint.
+            if(model is null) { 
+                return;
+            }
+
+            // Determine if the graph requires laying out within the specified viewportArea, i.e. if the previous call to Paint() 
+            // supplied the same viewportArea, then we can just use the layout positions determined previously.
+            if(viewportArea != _viewportArea)
+            {
+                // Layout the graph within the required viewport area, and record the area we used.
+                LayoutGraph(model, viewportArea);
+                _viewportArea = viewportArea;
+            }
+
+            // Paint the graph.
+            _graphPainter.PaintGraph(model, g, viewportArea, zoomFactor);
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void LayoutGraph(DirectedGraphViewModel model, Rectangle viewportArea)
+        {
+            // Determine 2D position for each node in the graph, within the viewport area/rectangle.
+            _layoutScheme.Layout(model.DirectedGraph, viewportArea.Size, model.NodePosByIdx, ref _layoutSchemeData);
+
+            // Translate the points if the viewport origin is not (0,0); the layout schemes assume an origin of (0,0).
+            if(viewportArea.Location != Point.Empty)
+            {
+                Point origin = viewportArea.Location;
+                Point[] nodePosByIdx = model.NodePosByIdx;
+
+                for(int i=0; i < model.NodePosByIdx.Length; i++) {
+                    nodePosByIdx[i].Offset(origin);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
--- src/SharpNeat.Windows/ViewportControl.cs
+++ src/SharpNeat.Windows/ViewportControl.cs
@@ -10,6 +10,7 @@
  * along with SharpNEAT; if not, see https://opensource.org/licenses/MIT.
  */
 using System;
+using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
@@ -19,13 +20,12 @@ using SharpNeat.Drawing;
 namespace SharpNeat.Windows
 {
     /// <summary>
-    /// A user control that provides a re-sizable area that can be painted to by an 
+    /// A user control that provides a resizable area that can be painted to by an 
     /// IViewportPainter.
     /// </summary>
-    public class Viewport : UserControl
+    public class ViewportControl : UserControl
     {
-        const PixelFormat ViewportPixelFormat = PixelFormat.Format24bppRgb;
-        static readonly Brush _brushBackground = new SolidBrush(Color.White);
+        const PixelFormat __viewportPixelFormat = PixelFormat.Format24bppRgb;
 
         #region Instance Fields
 
@@ -51,11 +51,11 @@ namespace SharpNeat.Windows
         /// </summary>
         public float ZoomFactor
         {
-            get { return _zoomFactor; }
+            get => _zoomFactor;
             set
             {   
                 _zoomFactor = value;
-                RefreshImage();
+                Refresh();
             }
         }
 
@@ -66,7 +66,7 @@ namespace SharpNeat.Windows
         /// <summary>
         /// Default constructor. Required for user controls.
         /// </summary>
-        public Viewport()
+        public ViewportControl()
         {
             // This call is required by the Windows.Forms Form Designer.
             InitializeComponent();
@@ -76,7 +76,7 @@ namespace SharpNeat.Windows
             _viewportArea = new Rectangle(0, 0, width, height);
 
             // Create a bitmap for the picturebox.
-            _image = new Bitmap(width, height, ViewportPixelFormat);           
+            _image = new Bitmap(width, height, __viewportPixelFormat);           
             pictureBox.Image = _image;
         }
 
@@ -86,7 +86,7 @@ namespace SharpNeat.Windows
         protected override void Dispose(bool disposing)
         {
             if(disposing) {
-                if(components != null) {
+                if(components is object) {
                     components.Dispose();
                 }
             }
@@ -110,8 +110,8 @@ namespace SharpNeat.Windows
             // 
             this.pictureBox.Dock = DockStyle.Fill;
             this.pictureBox.Location = new Point(0, 0);
-            this.pictureBox.Name = "pictureBox";
-            this.pictureBox.Size = new Size(216, 232);
+            this.pictureBox.Name = "pbx1";
+            this.pictureBox.Size = new Size(100, 100);
             this.pictureBox.TabIndex = 0;
             this.pictureBox.TabStop = false;
             this.pictureBox.SizeChanged += new EventHandler(this.PictureBox_SizeChanged);
@@ -120,32 +120,27 @@ namespace SharpNeat.Windows
             // Viewport
             // 
             this.Controls.Add(this.pictureBox);
-            this.Name = "Viewport";
-            this.Size = new Size(216, 232);
+            this.Name = "vpt1";
+            this.Size = new Size(100, 100);
             this.ResumeLayout(false);
-
         }
 
         #endregion
 
         #region Public Methods
 
-        /// <summary>
-        /// Refresh/repaint the image being displayed by the control.
-        /// </summary>
-        public void RefreshImage()
+        public override void Refresh()
         {
-            Graphics g = Graphics.FromImage(_image);
-            g.FillRectangle(_brushBackground, 0, 0, _image.Width, _image.Height);
-            g.SmoothingMode = SmoothingMode.AntiAlias;
-            g.CompositingQuality = CompositingQuality.AssumeLinear;
-
-            // If a painter has been assigned then paint the graph.
-            if(ViewportPainter is object) 
+            // If a painter has been assigned, then paint the graph.
+            if(ViewportPainter is object)
             {
+                Graphics g = Graphics.FromImage(_image);
+                g.SmoothingMode = SmoothingMode.AntiAlias;
+                g.CompositingQuality = CompositingQuality.AssumeLinear;
                 ViewportPainter.Paint(g, _viewportArea, _zoomFactor);
-                Refresh();
             }
+
+            base.Refresh();
         }
 
         #endregion
@@ -166,20 +161,21 @@ namespace SharpNeat.Windows
                 return;
             }
 
-            // If the viewport has grown beyond the size of the image then create a new image. 
+            // If the viewport has grown beyond the size of the image, then create a new image. 
             // Note. If the viewport shrinks we just paint on the existing (larger) image, this prevents unnecessary 
             // and expensive construction/destruction of Image objects.
             if(width > _image.Width || height > _image.Height) 
             {   
                 // Reset the image's size. We round up the nearest __imageSizeChangeDelta. This prevents unnecessary 
                 // and expensive construction/destruction of Image objects as the viewport is resized multiple times.
-                int imageWidth = (int)(Math.Ceiling((float)width / ImageSizeChangeDelta) * ImageSizeChangeDelta);
-                int imageHeight = (int)(Math.Ceiling((float)height / ImageSizeChangeDelta) * ImageSizeChangeDelta);
-                _image = new Bitmap(imageWidth, imageHeight, ViewportPixelFormat);
+                int imageWidth = (int)(MathF.Ceiling(width / ImageSizeChangeDelta) * ImageSizeChangeDelta);
+                int imageHeight = (int)(MathF.Ceiling(height / ImageSizeChangeDelta) * ImageSizeChangeDelta);
+                _image = new Bitmap(imageWidth, imageHeight, __viewportPixelFormat);
                 pictureBox.Image = _image;
             }
 
-            RefreshImage();
+            // Repaint the viewport/control.
+            Refresh();
         }
 
         private void PictureBox_DoubleClick(object sender, EventArgs e)
