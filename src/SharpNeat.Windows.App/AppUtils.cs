a89be44dd1c881203698c40eff301d16840bed50
colgreen <colin.green1@gmail.com>
2020-09-12 09:35:07 +0100

    Added UIAccessUtils.

--- src/SharpNeat.Windows.App/AppUtils.cs
+++ src/SharpNeat.Windows.App/AppUtils.cs
@@ -10,10 +10,7 @@
  * along with SharpNEAT; if not, see https://opensource.org/licenses/MIT.
  */
 using System;
-using System.Globalization;
 using System.Text.Json;
-using System.Windows.Forms;
-using log4net;
 using SharpNeat.Experiments;
 using SharpNeat.IO;
 using SharpNeat.Windows.App.Experiments;
@@ -22,8 +19,6 @@ namespace SharpNeat.Windows.App
 {
     internal static class AppUtils
     {
-        private static readonly ILog __log = LogManager.GetLogger(typeof(AppUtils));
-
         public static INeatExperiment<double> CreateAndConfigureExperiment(ExperimentInfo expInfo)
         {
             // Create an experiment factory.
@@ -35,34 +30,6 @@ namespace SharpNeat.Windows.App
             // Create an instance of INeatExperiment, configured using the supplied json config.
             INeatExperiment<double> experiment = factory.CreateExperiment(configDoc.RootElement);
             return experiment;
-        }
-
-        public static void SetValue(TextBox txtBox, int val)
-        {
-            txtBox.Text = val.ToString(CultureInfo.InvariantCulture);
-        }
-
-        public static void SetValue(TextBox txtBox, double val)
-        {
-            txtBox.Text = val.ToString(CultureInfo.InvariantCulture);
-        }        
-
-        public static int GetValue(TextBox txtBox, int defaultVal)
-        {
-            if(int.TryParse(txtBox.Text, out int tmp)) {
-                return tmp;
-            }
-            __log.ErrorFormat("Error parsing value of text field [{0}]", txtBox.Name);
-            return defaultVal;
-        }
-
-        public static double GetValue(TextBox txtBox, double defaultVal)
-        {
-            if(double.TryParse(txtBox.Text, out double tmp)) {
-                return tmp;
-            }
-            __log.ErrorFormat("Error parsing value of text field [{0}]", txtBox.Name);
-            return defaultVal;
         }
     }
 }
---
+++ src/SharpNeat.Windows.App/UIAccessUtils.cs
@@ -0,0 +1,50 @@
+﻿/* ***************************************************************************
+ * This file is part of SharpNEAT - Evolution of Neural Networks.
+ * 
+ * Copyright 2004-2020 Colin Green (sharpneat@gmail.com)
+ *
+ * SharpNEAT is free software; you can redistribute it and/or modify
+ * it under the terms of The MIT License (MIT).
+ *
+ * You should have received a copy of the MIT License
+ * along with SharpNEAT; if not, see https://opensource.org/licenses/MIT.
+ */
+using System.Globalization;
+using System.Windows.Forms;
+using log4net;
+
+namespace SharpNeat.Windows.App
+{
+    public static class UIAccessUtils
+    {
+        private static readonly ILog __log = LogManager.GetLogger(typeof(UIAccessUtils));
+
+        public static void SetValue(TextBox txtBox, int val)
+        {
+            txtBox.Text = val.ToString(CultureInfo.InvariantCulture);
+        }
+
+        public static void SetValue(TextBox txtBox, double val)
+        {
+            txtBox.Text = val.ToString(CultureInfo.InvariantCulture);
+        }        
+
+        public static int GetValue(TextBox txtBox, int defaultVal)
+        {
+            if(int.TryParse(txtBox.Text, out int tmp)) {
+                return tmp;
+            }
+            __log.ErrorFormat("Error parsing value of text field [{0}]", txtBox.Name);
+            return defaultVal;
+        }
+
+        public static double GetValue(TextBox txtBox, double defaultVal)
+        {
+            if(double.TryParse(txtBox.Text, out double tmp)) {
+                return tmp;
+            }
+            __log.ErrorFormat("Error parsing value of text field [{0}]", txtBox.Name);
+            return defaultVal;
+        }
+    }
+}
