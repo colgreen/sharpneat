93fb0a5586ad84d1bec354cfdcca46a35765ece4
colgreen <colin.green1@gmail.com>
2020-10-03 12:47:47 +0100

    Added new project SharpNeat.Tasks.Windows, and interfaces IExperimentUI, IExperimentUIFactory.

---
+++ src/SharpNeat.Tasks.Windows/IExperimentUI.cs
@@ -0,0 +1,34 @@
+﻿/* ***************************************************************************
+ * This file is part of SharpNEAT - Evolution of Neural Networks.
+ * 
+ * Copyright 2004-2020 Colin Green (sharpneat@gmail.com)
+ *
+ * SharpNEAT is free software; you can redistribute it and/or modify
+ * it under the terms of The MIT License (MIT).
+ *
+ * You should have received a copy of the MIT License
+ * along with SharpNEAT; if not, see https://opensource.org/licenses/MIT.
+ */
+using SharpNeat.Windows;
+
+namespace SharpNeat.Tasks.Windows
+{
+    public interface IExperimentUI
+    {
+        /// <summary>
+        /// Create a new Windows.Forms UI control for direct visualisation of the genome, i.e. the 
+        /// actual neural net nodes and weights.
+        /// </summary>
+        /// <returns>A new instance of <see cref="GenomeControl"/>.</returns>
+        /// <returns>A new instance of <see cref="GenomeControl"/>; or null if the experiment does not provide a genome control.</returns>
+        GenomeControl CreateGenomeControl();
+
+        /// <summary>
+        /// Create a new Windows.Forms UI control for visualisation of task. E.g. for a prey capture task
+        /// we might have a control that show the prey capture world, and how the current best genome peforms
+        /// in that world.
+        /// </summary>
+        /// <returns>A new instance of <see cref="GenomeControl"/>; or null if the experiment does not provide a task control.</returns>
+        GenomeControl CreateTaskControl();
+    }
+}
---
+++ src/SharpNeat.Tasks.Windows/IExperimentUIFactory.cs
@@ -0,0 +1,29 @@
+﻿/* ***************************************************************************
+ * This file is part of SharpNEAT - Evolution of Neural Networks.
+ * 
+ * Copyright 2004-2020 Colin Green (sharpneat@gmail.com)
+ *
+ * SharpNEAT is free software; you can redistribute it and/or modify
+ * it under the terms of The MIT License (MIT).
+ *
+ * You should have received a copy of the MIT License
+ * along with SharpNEAT; if not, see https://opensource.org/licenses/MIT.
+ */
+using System.Text.Json;
+
+namespace SharpNeat.Tasks.Windows
+{
+    /// <summary>
+    /// Represents a factory of <see cref="IExperimentUI"/>.
+    /// </summary>
+    public interface IExperimentUIFactory
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="IExperimentUI"/> using the experiment
+        /// configuration settings from the provided json object model.
+        /// </summary>
+        /// <param name="configElem">Experiment config in presented as json object model.</param>
+        /// <returns>A new instance of <see cref="IExperimentUI{T}"/>.</returns>
+        IExperimentUI CreateExperimentUI(JsonElement configElem);
+    }
+}
