d11c5d992dabc706a1c73564ec24872b0aca0ea8
colgreen <colin.green1@gmail.com>
2021-04-11 21:00:18 +0100

    Removed whitespace.

--- src/SharpNeat/Graphs/Acyclic/LayerInfo.cs
+++ src/SharpNeat/Graphs/Acyclic/LayerInfo.cs
@@ -15,10 +15,10 @@ namespace SharpNeat.Graphs.Acyclic
     /// <summary>
     /// Represents a node and connection index that represent the last node and connection in a given layer
     /// in an acyclic graph.
-    /// 
+    ///
     /// The nodes and connections on an acyclic graph are ordered by the layer they are in. For more details
     /// on how the layers are determined/defined see <see cref="DirectedGraphAcyclic"/>.
-    /// 
+    ///
     /// Connections are defined as being in the same layer as their source node.
     /// </summary>
     public readonly struct LayerInfo
@@ -30,7 +30,7 @@ namespace SharpNeat.Graphs.Acyclic
         public int EndNodeIdx { get; }
 
         /// <summary>
-        /// Specifies the last connection in the current layer. Specifically, the index of that 
+        /// Specifies the last connection in the current layer. Specifically, the index of that
         /// connection plus one.
         /// </summary>
         public int EndConnectionIdx { get; }
@@ -42,7 +42,7 @@ namespace SharpNeat.Graphs.Acyclic
         /// </summary>
         /// <param name="endNodeIdx">Specifies the last node in the current layer. Specifically, the index of that
         /// node plus one.</param>
-        /// <param name="endConnectionIdx">Specifies the last connection in the current layer. Specifically, the index of that 
+        /// <param name="endConnectionIdx">Specifies the last connection in the current layer. Specifically, the index of that
         /// connection plus one.</param>
         public LayerInfo(int endNodeIdx, int endConnectionIdx)
         {
