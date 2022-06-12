5b8d4242cd598ad345459c3b4162e5f783408811
Colin Green <colin.green1@gmail.com>
2022-06-12 09:18:43 +0100

    Deleted dead class ActivationFunctionRow.

---
+++ src/SharpNeat/NeuralNets/IO/NeuralNetConverter.cs
@@ -0,0 +1,32 @@
+﻿// This file is part of SharpNEAT; Copyright Colin D. Green.
+// See LICENSE.txt for details.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpNeat.IO.Models;
+
+namespace SharpNeat.NeuralNets.IO;
+
+/// <summary>
+/// Static methods for converting between neural network types and <see cref="NetFileModel"/>.
+/// </summary>
+public static class NeuralNetConverter
+{
+
+    public static IBlackBox<double> ToNeuralNet(
+        NetFileModel model)
+    {
+
+
+
+
+    }
+
+
+
+
+
+
+}
