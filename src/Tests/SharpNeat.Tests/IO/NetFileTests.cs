5996013fe17659afee8c1b9bea0700ff62678164
Colin Green <colin.green1@gmail.com>
2022-06-15 22:45:13 +0100

    - Added NeatGenomeConverterTests. - Deleted NeatGenomeIOTests, as the equivalent test is performed by NeatGenomeSaverTests.

--- src/Tests/SharpNeat.Tests/IO/NetFileTests.cs
+++ src/Tests/SharpNeat.Tests/IO/NetFileTests.cs
@@ -66,22 +66,3 @@ public class NetFileTests
     }
 }
 
-internal static class NetFileTestExtensions
-{
-    public static void Validate(
-        this ConnectionLine connLine,
-        int srcId, int tgtId, double weight)
-    {
-        connLine.SourceId.Should().Be(srcId);
-        connLine.TargetId.Should().Be(tgtId);
-        connLine.Weight.Should().Be(weight);
-    }
-
-    public static void Validate(
-        this ActivationFnLine actFnLine,
-        int id, string code)
-    {
-        actFnLine.Id.Should().Be(id);
-        actFnLine.Code.Should().Be(code);
-    }
-}
---
+++ src/Tests/SharpNeat.Tests/Neat/Genome/NeatGenomeTestExtensions.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using SharpNeat.Graphs;
+
+namespace SharpNeat.Neat.Genome;
+
+internal static class NeatGenomeTestExtensions
+{
+    public static void Validate(
+        this DirectedConnection conn,
+        int srcId, int tgtId)
+    {
+        conn.SourceId.Should().Be(srcId);
+        conn.TargetId.Should().Be(tgtId);
+    }
+}
