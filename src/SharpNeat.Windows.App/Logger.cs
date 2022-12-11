3109317faaf7012a141a5d9f10074efcf041d904
colgreen <colin.green1@gmail.com>
2022-12-11 23:08:57 +0000

    Add an empty log line to BindingList, as it appears the bound ListBox cannot handle having zero items in bound-to data (it throws an ArgumentOutOfRangeException when the first LogItem is added).

--- src/SharpNeat.Windows.App/Logger.cs
+++ src/SharpNeat.Windows.App/Logger.cs
@@ -22,6 +22,7 @@ internal sealed class Logger
     static Logger()
     {
         __bindingList = new BindingList<LogItem>(new LogBuffer(500));
+        __bindingList.Add(new LogItem(""));
     }
 
     /// <summary>
