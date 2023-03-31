66e8ac2ca099cca066f3f2a3e6e34d0fdb0429ab
Zia Siddique <siddiquezia@hotmail.com>
2023-03-31 16:47:17 +0100

    Add Bitwise ReLU to the monotonic function tests

--- src/Tests/SharpNeat.Tests/NeuralNets/Double/ActivationFunctions/ActivationFunctionMonotonicityTests.cs
+++ src/Tests/SharpNeat.Tests/NeuralNets/Double/ActivationFunctions/ActivationFunctionMonotonicityTests.cs
@@ -19,6 +19,7 @@ public class ActivationFunctionMonotonicityTests
         AssertMonotonic(new PolynomialApproximantSteep(), true);
         AssertMonotonic(new QuadraticSigmoid(), false);
         AssertMonotonic(new ReLU(), false);
+        AssertMonotonic(new BitwiseReLU(), false);
         AssertMonotonic(new ScaledELU(), true);
         AssertMonotonic(new SoftSignSteep(), true);
         AssertMonotonic(new SReLU(), true);
