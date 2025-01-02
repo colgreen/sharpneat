aa3504d4eb630542274b4f7970bf779a2163c051
Colin Green <colin.green1@gmail.com>
2025-01-02 20:11:00 +0000

    Reverted efic binary11 task to use of double precision floats (for now)

--- src/EfficacySampler/Program.cs
+++ src/EfficacySampler/Program.cs
@@ -35,7 +35,7 @@ sealed class Program
             .CreateLogger();
 
         // Create and configure a NEAT experiment instance.
-        var experiment = InitExperiment<float>(experimentId);
+        var experiment = InitExperiment<double>(experimentId);
         if(experiment is null)
             return;
 
